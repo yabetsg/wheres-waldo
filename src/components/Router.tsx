8d9c2ca7bfe7dd64508731d1887b7471ace5b8f4
yabets <yabets.g2@gmail.com>
2023-07-10 14:20:34 -0400

    Update Router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -8,7 +8,7 @@ export const Router = () => {
       <Routes>
         <Route path="/leaderboard" element={<Leaderboard />}></Route>
         <Route path="/leaderboard/" element={<Leaderboard />}></Route>
-        <Route path="/" element={<Game />}></Route>
+        <Route path="/wheres-waldo/" element={<Game />}></Route>
       </Routes>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/https://github.com/yabetsg/wheres-waldo/",
   plugins: [react()],
 })
